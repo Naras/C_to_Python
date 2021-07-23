3a43211f919f6d92384843cd646fb35680884321
Naras <narasimhanmg@gmail.com>
2021-07-23 18:55:45 +0530

    Added code for #define

--- parse_moderately_complex_inputs.py
+++ parse_moderately_complex_inputs.py
@@ -1,3 +1,4 @@
+__author__ = 'NarasMG'
 import re #, codecs #, icecream as ic
 import ply.lex as lex
 import ply.yacc as yacc
@@ -53,7 +54,8 @@ reserved={
     'unsigned': 'UNSIGNED',
     'and': 'AND', 'or': 'OR', 'not': 'NOT',
     'typedef': 'TYPEDEF',
-    '#include': 'INCLUDE'
+    '#include': 'INCLUDE',
+    '#define': 'DEFINE'
 }
 tokens += reserved.values()
 # Regular expression rules for simple tokens
@@ -105,6 +107,7 @@ t_NOT = r"\!"
 t_TYPEDEF = r"typedef\s*"
 t_INCLUDE = r"\#include*"
 t_DOT = r"\."
+t_DEFINE = r"\#define*"
 
 def t_POINTER_TO_STRUCT(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*\-\>[a-zA-Z_][a-zA-Z0-9_]*'
@@ -390,7 +393,10 @@ def p_statement_default(p):
     case_var_stmt_pairs.append({"default":p[3]})
 def p_statement_break(p):
     'statement : BREAK'
-    p[0] = "break;"
+    p[0] = "break"
+def p_statement_continue(p):
+    'statement : CONTINUE'
+    p[0] = "continue"
 def p_statement_while(p):
     'statement : WHILE LPAREN expression RPAREN statement'
     p[0] = ("while " + p[3] + ":" + p[5]).replace("NULL", "None")
@@ -452,6 +458,11 @@ def p_comment_multiline(p):
 def p_comment_singleline(p):
     'statement : COMMENT_SINGLE_LINE'
     p[0] = "# " + p[1][3:]
+def p_statement_define(p):
+    '''statement : DEFINE ID NUMBER
+                    | DEFINE ID LITERAL_APOSTROPHE
+                    | DEFINE ID LITERAL_QUOTE'''
+    p[0] = p[2] + " = " + str(p[3])
 # Error rule for syntax errors
 def p_error(p):
     print('Syntax error at "%s"' % p.value)
@@ -602,25 +613,27 @@ if __name__ == '__main__':
     stmt_include = "#include <stdio.h>\n"
     stmt_include2 = '#include "sengen1.h"\n'
     stmt_include3 = '#include "data.h"\n'
-    stmt_typedef = "typedef struct{unsigned char *pratipadika[20]; unsigned char *erb[20];         /* End Removed Base */ " \
-                "int wordNum[20]; int numofNouns;} SUBANTA_DATA;"
+    stmt_define = "#define KARTHARI    0; #define KARMANI     1; #define FULLSTOP  'ê'; #define eof       255"
+    stmt_typedef = "typedef struct { int vibhakti[20]; int vacana[20]; int linga[20]; int purusha[20]; unsigned char *subanta[20]; unsigned char *pratipadika[20]; unsigned char *erb[20];         /* End Removed Base */; int wordNum[20]; int numofNouns; } SUBANTA_DATA;"
+    stmt_typedef_many = "typedef struct { int vibhakti[20]; int vacana[20]; int linga[20]; int purusha[20]; unsigned char *subanta[20]; unsigned char *pratipadika[20]; unsigned char *erb[20];         /* End Removed Base */; int wordNum[20]; int numofNouns; } SUBANTA_DATA;  typedef struct { int dhatuVidha[10]; int prayoga[10]; int lakara[10]; int purusha[10]; int vacana[10]; int gana[10]; int padi[10]; int karma[10]; int it[10]; unsigned char *tiganta[10]; unsigned char *dhatu[10]; unsigned char *nijdhatu[10]; unsigned char *sandhatu[10]; unsigned char *artha[10]; unsigned char *err[10];    /* End Removed Root */; int wordNum[10]; int numofVerbs; } TIGANTA_DATA;  typedef struct { int vibhakti[20]; int vacana[20]; int linga[20]; int prayoga[20]; int krdType[20]; int dhatuVidha[20]; int purusha[20]; int gana[20]; int padi[20]; int karma[20]; int it[20]; unsigned char *krdanta[20]; unsigned char *pratipadika[20]; unsigned char *erb[20];            /* end removed base of krdanta */; unsigned char *dhatu[20]; unsigned char *nijdhatu[20]; unsigned char *sandhatu[20]; unsigned char *artha[20]; int wordNum[20]; int numofKrdantas; } KRDANTA_DATA;  typedef struct { unsigned char *avyaya[30]; int wordNum[30]; int numofAvyayas; } AVYAYA_DATA;  typedef struct { int dhatuVidha[20]; int gana[20]; int padi[20]; int karma[20]; int it[20]; int krdavType[20]; unsigned char *krdavyaya[20]; unsigned char *dhatu[20]; unsigned char *nijdhatu[20]; unsigned char *sandhatu[20]; unsigned char *artha[20]; int wordNum[20]; int numofKrdavyayas; } KRDAV_DATA;  typedef struct { unsigned char *word[20]; int vibhakti[20]; int vacana[20]; int purusha[20]; int linga[20]; int wordPos[20]; int numofWords; } VIBHAKTI;  typedef struct { unsigned char *verb; unsigned char *dhatu; int purusha; int vacana; int prayoga; int karma; int wordPos; } VERB;  typedef struct { unsigned char *krdanta; int vibhakti; int vacana; int linga; int prayoga; int karma; int krdType; } PARTICIPLE;  typedef struct { unsigned char *sentence; unsigned char *idens[100]; int numofIdens; } RECORD;  typedef struct { unsigned char *iden[30]; int numofIdens; } WORD;  typedef struct { unsigned char *word[15]; int numofWords; } TYPE;"
     stmt_var_decl_array = "unsigned char list[]={'ÈÞÏèÔÚÁèØ', '¤ÈÏÚÁèØ', 'ÄÛÆ', 'ÏÚÂèÏÛ', '¤ØåÏÚÂèÏ', '×ÈèÂÚØ', 'È³èÖ', 'ÌÚ×', '×¢ÔÂè×Ï'};"
     stmt_func_def_vibmenu_full = "int choice(char type,unsigned char *word,unsigned char voice[],int pos,VIBAK *tvibptr,FILE *afp,long fl,unsigned char *VerbMean) { int yes=0,success=1;  while(1) { if((tvibptr->stype =='1' && strcmp(tvibptr->specf,'dative')==0 ) || tvibptr->stype =='5' || tvibptr->stype=='2'|| tvibptr->stype=='4') { /* Check for case where there is only a single meaning for ¸ÂÝÏèÂÜ ÔÛË³èÂÛ */ yes=findverb(voice,tvibptr->sword,tvibptr,afp,fl,VerbMean);  if(tvibptr->stype=='2' && tvibptr->matnoun !=1 ) { switch(tvibptr->spos) { case 0: if(tvibptr->semlinga==0) strcat(tvibptr->arthaword,'×Ú '); if(tvibptr->semlinga==1) strcat(tvibptr->arthaword,'×£ '); if(tvibptr->semlinga==2) strcat(tvibptr->arthaword,'ÂèÂ '); break; case 1: strcat(tvibptr->arthaword,'ÂÆèÆÛÖè¾³ÏèÌÂÚÆÛÏÞÈ³ '); break; case 2: strcat(tvibptr->arthaword,'ÆÛÖè¾³ÏÁÂÚÆÛÏÞÈ³ '); break; case 3: strcat(tvibptr->arthaword,'ÆÛÖè¾×ÌèÈèÏÄÚÆÂÚÆÛÏÞÈ³ '); break; case 4: strcat(tvibptr->arthaword,'ÆÛÖè¾ÚÈÚÄÚÆÂÚÆÛÏÞÈ³ '); break; case 5: strcat(tvibptr->arthaword,'ÆÛÖè¾ÚÅÛ³ÏÁÂÚÆÛÏÞÈ³ '); break; } } if(tvibptr->stype == '2' || tvibptr->stype =='4' || tvibptr->stype=='5') success= 0;  } if(tvibptr->stype =='1' && (strcmpi(tvibptr->specf,'object')==0)) {        /* Check for case where there is only a single meaning for ÄèÔÛÂÜÍÚ ÔÛË³èÂÛ */ yes=findverb(voice,tvibptr->sword,tvibptr,afp,fl,VerbMean); }   /* If not in above case following steps lead to menu display for    selection based on type of vibhakti */  if(tvibptr->stype =='1')  {  switch(tvibptr->spos)  { case 0: if(strcmpi(voice,'kartari') ==0) strcpy(tvibptr->arthaword,tvibptr->sword); if(strcmpi(voice,'karmani') ==0) { strcpy(tvibptr->arthaword,tvibptr->bword); strcat(tvibptr->arthaword,'ÆÛÖè¾³ÏèÂßÂÚÆÛÏÞÈ³ '); } break;  case 1: if(strcmpi(voice,'kartari') ==0) { strcpy(tvibptr->arthaword,tvibptr->bword); strcat(tvibptr->arthaword,'ÆÛÖè¾³ÏèÌÂÚÆÛÏÞÈ³ '); } if(strcmpi(voice,'karmani') ==0) { strcpy(tvibptr->arthaword,tvibptr->sword); } break;  case 2: strcpy(tvibptr->arthaword,tvibptr->bword); strcat(tvibptr->arthaword,'ÆÛÖè¾³ÏÁÂÚÆÛÏÞÈ³ '); break;  case 3: strcpy(tvibptr->arthaword,tvibptr->bword); strcat(tvibptr->arthaword,'ÆÛÖè¾×ÌèÈèÏÄÚÆÂÚÆÛÏÞÈ³ '); break;  case 4: strcpy(tvibptr->arthaword,tvibptr->bword); strcat(tvibptr->arthaword,'ÆÛÖè¾ÚÈÚÄÚÆÂÚÆÛÏÞÈ³ '); break;  case 6: strcpy(tvibptr->arthaword,tvibptr->bword); strcat(tvibptr->arthaword,'×ÌèÊÆèÅÛ '); break;  case 5: strcpy(tvibptr->arthaword,tvibptr->bword); strcat(tvibptr->arthaword,'ÆÛÖè¾ÚÅÛ³ÏÁÂÚÆÛÏÞÈ³ '); break; }  }  if (tvibptr->next != NULL) tvibptr=tvibptr->next;  else  break; } return success; }"
 
     pattern_crlf, pattern_spaces_2_or_more, pattern_tabs, pattern_c_strcmp, pattern_c_strcpy, pattern_c_strcat, \
-    pattern_include, pattern_not_include,  pattern_nl, pattern_star_slash = \
+    pattern_include, pattern_define,  pattern_nl, pattern_star_slash, pattern_star_slash_semicolon = \
         re.compile(r"\r\n"), re.compile(" +"), re.compile("\t+"), re.compile("strcmpi?\((.+?),(.+?)\)\s*==\s*0"), \
         re.compile("strcpy\((.+?)\s*,\s*(.+?)\)"), re.compile("strcat\(\s*(.+?)\s*,\s*(.+?)\s*\)"),\
-        re.compile(r"#include(.+)"), re.compile(r"'^((?!#include).)*$'"), re.compile(r"\n$"), re.compile(r"\*\/")
+        re.compile(r"#include(.+)"), re.compile(r"#define(.+)"), re.compile(r"\n$"), re.compile(r"\*\/"),  re.compile(r"\*\/;")
 
     # Give the lexer some input
 
-    '''for statement in [stmt_comment, stmt_var_decl_initialized, stmt_var_decl_array, stmt_assignment, stmt_func_decl_simple, stmt_func_decl_complex, stmt_func_decl_complex1, stmt_func_decl_complex2, stmt_func_def_complex1, stmt_func_def_complex2, stmt_assignment_func, stmt_if_assign, stmt_if_assign2, stmt_if_assign3,  stmt_strcmp_cpy_cat, stmt_switch_case, stmt_switch_case1, stmt_switch_case2, stmt_switch_case22,  stmt_switch_case3, stmt_while, stmt_while_complex1, stmt_while_complex2, stmt_while_complex3, stmt_include, stmt_include2, stmt_include3, stmt_typedef, stmt_func_def_vibmenu_full]:
-    # for statement in [stmt_typedef]:
+    # for statement in [stmt_comment, stmt_var_decl_initialized, stmt_var_decl_array, stmt_assignment, stmt_func_decl_simple, stmt_func_decl_complex, stmt_func_decl_complex1, stmt_func_decl_complex2, stmt_func_def_complex1, stmt_func_def_complex2, stmt_assignment_func, stmt_if_assign, stmt_if_assign2, stmt_if_assign3,  stmt_strcmp_cpy_cat, stmt_switch_case, stmt_switch_case1, stmt_switch_case2, stmt_switch_case22,  stmt_switch_case3, stmt_while, stmt_while_complex1, stmt_while_complex2, stmt_while_complex3, stmt_define, stmt_include, stmt_include2, stmt_include3, stmt_typedef_many, stmt_func_def_vibmenu_full]:
+    for statement in [stmt_func_decl_simple]:
         case_var_stmt_pairs, typedef_vars = [{}], [{}]
-        print("C statement: %s \nPython statement: %s" % (statement, main(pattern_star_slash.sub("*/;", statement)))) # */ to */;'''
+        if not pattern_star_slash_semicolon.findall(statement): statement = pattern_star_slash.sub("*/;", statement)
+        print("C statement: %s \nPython statement: %s" % (statement, main(statement))) # */ to */;
 
-    f = open("VIBMENU.C") #codecs.open("VIBMENU.C", encoding="utf-8")
+    '''f = open("VIBMENU.C") #codecs.open("VIBMENU.C", encoding="utf-8")
     csource = f.readlines()
     f.close()
     statement_asis = pattern_crlf.sub("\n", " ".join(csource))
@@ -638,4 +651,20 @@ if __name__ == '__main__':
     for statement in includes.split("\n"): include_statements.append(main(statement))
     print("C includes: %s \nPython imports: %s"%(includes, "\n".join(include_statements)))
     # for statement in rest.split('\n'): print("C statement: %s \nPython statement: %s"%(statement, main(statement)))
-    print("C statement: %s \nPython statement: %s"%(rest, main(rest)))
+    print("C statement: %s \nPython statement: %s"%(rest, main(rest)))'''
+
+    f = open("I:\VBtoPython\Amarakosha\Senanal\SYNTAX.H")
+    csource = f.readlines()
+    f.close()
+    statement_asis = pattern_crlf.sub("\n", " ".join(csource))
+    defines = '\n '.join(["#define" + p for p in pattern_define.findall(statement_asis)])
+    case_var_stmt_pairs, typedef_vars = [{}], [{}]
+    # print("includes %s\nasis %s"%(includes, statement_asis))
+    splits = statement_asis.split(defines)
+    rest = pattern_crlf.sub("\n", splits[1])
+    rest = pattern_star_slash.sub("*/;", rest)
+    include_statements = []
+    for statement in defines.split("\n"): include_statements.append(main(statement))
+    print("C defines: %s \nPython inits: %s" % (defines, "\n".join(include_statements)))
+    # exit(1)
+    print("C statement: %s \nPython statement: %s" % (rest, main(rest)))
